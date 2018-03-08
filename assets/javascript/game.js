5d4a5f8a018b40990898257e7d57466b1c9ef5ca
jrizza88 <jetorres88@gmail.com>
2018-03-07 22:14:32 -0500

    getting closer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,11 +14,14 @@
     var selectedWord;
     var hiddenWord =[];
      var letterCounter;
+     var revealLetter =[];
+     var correctWord =[];
 
 // use math.floor to randomly select word from "words" variable
 var randomizedWord = words[Math.floor(Math.random() * words.length)];
 console.log(randomizedWord);
  document.getElementById('guessesLeft').innerHTML = guessesLeft;
+
 
 // start game by using on keyUp function
 document.onkeyup = function(event){
@@ -26,12 +29,13 @@ document.onkeyup = function(event){
     // console.log(userGuess);
 // userGuess shows the number of times guesses were inputted. 
 letterInput.push(userGuess);
+
 document.getElementById('lettersGuessed').innerHTML = letterInput;
 // letterInput.push(correctGuess);
 // meant to push into HTML
  // selectedWord = words[randomizedWord];
  // selectedWord = randomizedWord[i];
-  hiddenWord = randomizedWord.replace(/\S/gi, '_');
+  hiddenWord = randomizedWord.replace(/\S/gi, '_ ');
   document.getElementById("mysteryWord").innerHTML = hiddenWord;
 
 // var counter = words;
@@ -44,42 +48,105 @@ document.getElementById('lettersGuessed').innerHTML = letterInput;
 
     var inputLetter = 0;
     
-  // compare user input to randomCountryWord
-      for (var i = 0; i <= randomizedWord.length; i++) {
-        // make sure anything not in the alphabet can be selected
-        if (userGuess < "A" || userGuess > "Z") {
-          console.log("you did not guess a letter");
-              return false;
-        }
-        // finally getting somewhere with selectedWord
-          else if(randomizedWord[i] === userGuess){
-          // correctLetter[i] = userGuess;
-          correctLetter[i] = userGuess;
-          inputLetter++;
-            // letterInput.push(correctGuess); - this put in the numbers
-            document.getElementById('correctLetter').innerHTML = correctLetter;
-            console.log("you choose a correct letter!: " + userGuess);
-            // console.log("it's a correct guess!: " + correctGuess);
+         // compare user input to randomCountryWord
+              for (var i = 0; i <= randomizedWord.length; i++) {
+                // make sure anything not in the alphabet can be selected
+                if (userGuess < "A" || userGuess > "Z") {
+                  console.log("you did not guess a letter");
+
+                      return false;
+                }
+
+                // finally getting somewhere with selectedWord
+                  else if(randomizedWord[i] === userGuess){
+                  // correctLetter[i] = userGuess;
+     
+                  correctLetter[i] = userGuess;
+                  inputLetter++;
+                   // revealLetter = randomizedWord.replace(/\S/i, userGuess);
+                   hiddenWord = randomizedWord.replace(/\S/i, userGuess);
+                   // selectedWord = randomizedWord.replace(/\s+/gi, '');
+                   // var spaceIssue = randomizedWord.replace(/\s/g, userGuess);
+                  // revealLetter = hiddenWord.replace(/'_'/i, \S/);
+              
+                  console.log("revealLetter: " + revealLetter);
+                    // letterInput.push(correctGuess); - this put in the numbers
+                    document.getElementById('correctLetter').innerHTML = correctLetter.join("");
+                    // document.getElementById('mysteryWord').innerHTML = revealLetter;
+                    // document.getElementById('mysteryWord').innerHTML = hiddenWord;
+                    console.log("you choose a correct letter!: " + userGuess);
+                    console.log("inputLetter: " + inputLetter);
+                    // console.log("it's a correct guess!: " + correctGuess);
+                    // if (!letterFound) {
+
+                    // }
+                  } 
+              }
+
+// SAVE THIS WHEN YOU REVEAL WORD FOR SOME REASON!!!
+              // revealLetter = randomizedWord.replace(/\S/i, correctLetter[i]);
+
+                if (inputLetter === 0)  {
+                      // userGuess !== randomizedWord[i];
+                    guessesLeft--;
+                    console.log("guessesLeft: " + guessesLeft);
+                    console.log(inputLetter);
+                    document.getElementById('guessesLeft').innerHTML = guessesLeft;
+                  }
+
+
+            correctWord = correctLetter.join("");
+
+
+        if (correctWord === randomizedWord) {
+          wins++;
+          document.getElementById("win-counter").innerHTML = wins;
+          revealLetter = randomizedWord.replace(/\S/i, correctLetter[i]);
+          document.getElementById('mysteryWord').innerHTML = revealLetter;
+          alert("You won this round!!!");
+          location.reload();
+        } 
+
+          else if (guessesLeft === 9){
+            console.log("you have 9 guesses left!");
           }
-            // console.log(letterCounter)
-        // else if (userGuess !== randomizedWord[i]){
-        //   else if (userGuess !== randomizedWord[i])  {
-        //     // userGuess !== randomizedWord[i];
-        //   guessesLeft--;
-        //   document.getElementById('guessesLeft').innerHTML = guessesLeft;
-        //   console.log("wrong guess");
-        //   console.log("guessesLeft: " + guessesLeft);
-        // }
 
-      }
+           else if (guessesLeft === 8){
+            console.log("you have 8 guesses left!");
+          }
+           else if (guessesLeft === 7){
+            console.log("you have 7 guesses left!");
+          }
 
-      if (inputLetter === 0)  {
-            // userGuess !== randomizedWord[i];
-          guessesLeft--;
-          console.log("guessesLeft: " + guessesLeft);
-          console.log(inputLetter);
-          document.getElementById('guessesLeft').innerHTML = guessesLeft;
-        }
+           else if (guessesLeft === 6){
+            console.log("you have 6 guesses left!");
+          }
+
+           else if (guessesLeft === 5){
+            console.log("you have 5 guesses left!");
+          }
+           else if (guessesLeft === 4){
+            console.log("you have 4 guesses left!");
+          }
+           else if (guessesLeft === 3){
+            console.log("you have 3 guesses left!");
+          }
+           else if (guessesLeft === 2){
+            console.log("you have 2 guesses left!");
+          }
+           else if (guessesLeft === 1){
+            console.log("you have 1 guesses left!");
+          }
+
+          else if(guessesLeft === 0){
+            console.log("You lose!")
+
+          }
+
+          else if(guessesLeft === -1){
+            location.reload();
+          }
+
 
   }; // closing tag of function event
 
